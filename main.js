56f8ff0265e60f8b4cdbc041db82642305a1edd9
ahmed salih <ahmed.elabid.elsalih@gmail.com>
2022-04-05 14:25:30 +0300

    this is task from kodluyoruz bootcamp

---
+++ main.js
@@ -0,0 +1,26 @@
+// fetch( 'https://randomuser.me/api/')
+// .then(function(perponse){
+//     return perponse.json();
+// })
+// .then(function(data){
+//     console.log(data)
+// })
+//  old way
+
+async function giveMe(){
+    const response = await fetch(`https://randomuser.me/api/1.3`);
+    const myData = await response.json();
+    const sc = myData.results[0].picture.large;
+    const {title , first , last} = myData.results[0].name;
+    const{country , city }= myData.results[0].location
+    const age = myData.results[0].dob.age ;
+    const{email , phone , cell }= myData.results[0]
+    document.querySelector("img").src = sc;
+    document.getElementById("name").innerHTML = ` ${first} ${last}`;
+    document.getElementById("age").innerHTML = `Age: ${age} `;
+    document.getElementById("counter").innerHTML = `Counter :  ${country}  / ${city}  `;
+    document.getElementById("cell").innerHTML = `   ${phone}  / ${email}  `;
+    document.getElementById("code").innerHTML = ` ID Code : ${cell}  `;
+}
+giveMe()
+
